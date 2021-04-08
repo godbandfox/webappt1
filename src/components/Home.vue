1c572ab379261faccb8cb485f9de7446dad10959
Fxy <fstxizya@qq.com>
2021-04-08 09:31:56 +0800

    init vue net163

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class = "home">
+    <van-button type = "primary">主要按键</van-button>
+    <van-stepper v-model = "value"></van-stepper>
+    <van-search placeholder = "搜索" v-model = "searchVal"/>
+  </div>
+</template>
+
+<script>
+import '@/vantui'
+
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      value: 10,
+      searchVal: '',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
